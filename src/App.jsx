e8640e69865373d376e66d9324ee1b6556ddbe80
Robert Toroitich <robertojnr2802@gmail.com>
2026-05-15 15:19:21 +0300

    Added background component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
+import HeroGeometric from './components/Background'
 
 function App() {
 
   return ( 
-  <>
-  </>
+  <div className='hero-section'>
+    <HeroGeometric
+      title1="Robert's"
+      title2="Coffee Shop"
+      description="Enjoy the finest coffee from Kenya's leading coffee shop!"
+      color1="#3B82F6"
+      color2="#F0F9FF"
+      speed={1.6}
+    />
+  </div>
   )
 }
 
